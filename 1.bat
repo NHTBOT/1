0013190d3b797c336312bb17f9e3d4e76a610bd2
NHTBOT <140639828+NHTBOT@users.noreply.github.com>
2023-09-30 10:39:12 +0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,3 +1,3 @@
 echo NHT hacked > C:\Users\jimmi\OneDrive\Desktop\NHT.txt
-curl https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe -o putty.exe
+curl https://the.earth.li/~sgtatham/putty/0.79/w64/putty.exe -o putty.exe
 putty.exe
